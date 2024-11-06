12b61ed7cefeb7712fe0c7afcbb242ba116a6402
minseok <alstjrwjd99@naver.com>
2024-11-06 12:21:18 +0900

    feat(Inventory): Add inventory controller

--- src/controllers/ConvenienceController.js
+++ src/controllers/ConvenienceController.js
@@ -1,11 +1,13 @@
 import { InputView } from "../views/InputView.js";
 import { OutputView } from "../views/OutputView.js";
 import { fileController } from "./FileController.js";
+import InventoryController from "./InventoryController.js";
 
 export default class ConvenienceController {
     async start() {
         const data = await fileController.loadProducts();
         const products = fileController.splitProductsInfo(data);
+        new InventoryController(products);
         OutputView.printProducts(products);
         InputView.readItem();
     }
---
+++ src/controllers/InventoryController.js
@@ -0,0 +1,17 @@
+export default class InventoryController {
+    #generalProduct = [];
+    #promotionProduct = [];
+
+    constructor(totalProduct) {
+        totalProduct.forEach((product) => {
+            if (product.promotion === 'null') {
+                this.#generalProduct.push(product);
+            }else {
+                this.#promotionProduct.push(product);
+            }
+        });
+    }
+
+    
+
+}
